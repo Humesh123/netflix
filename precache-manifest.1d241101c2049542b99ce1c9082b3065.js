cbc31985a1e39eddce9a3f224fafe0ce76315e45
Humesh123 <humesh.sindpure@9abusiness.com>
2020-07-20 18:36:23 +0530

    Updates

---
+++ precache-manifest.1d241101c2049542b99ce1c9082b3065.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33c68e65f83e3ec0f81d352d1b738bd",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "a82b9441e424f7ad88c1",
+    "url": "/netflix/static/css/main.f9606386.chunk.css"
+  },
+  {
+    "revision": "7fd5d87cde861970d6fc",
+    "url": "/netflix/static/js/2.e62ebbaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix/static/js/2.e62ebbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82b9441e424f7ad88c1",
+    "url": "/netflix/static/js/main.4ce21b77.chunk.js"
+  },
+  {
+    "revision": "2dddb01aa0a4e9e8f0b8",
+    "url": "/netflix/static/js/runtime-main.5625af0b.js"
+  },
+  {
+    "revision": "e549d9d1dfdb2580603df3edd9b87503",
+    "url": "/netflix/static/media/Strange.e549d9d1.jpg"
+  },
+  {
+    "revision": "f9b92e79df7bd2e9201a20380e4f9bc5",
+    "url": "/netflix/static/media/dark.f9b92e79.jpg"
+  }
+]);
